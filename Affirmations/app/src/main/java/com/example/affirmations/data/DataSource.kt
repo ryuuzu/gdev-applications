46cca7d96b8f1befe30f8a94ed0db47397c8906c
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-09-22 19:35:31 +0545

    Set up the list of data

---
+++ Affirmations/app/src/main/java/com/example/affirmations/data/DataSource.kt
@@ -0,0 +1,21 @@
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class DataSource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmations1),
+            Affirmation(R.string.affirmations2),
+            Affirmation(R.string.affirmations3),
+            Affirmation(R.string.affirmations4),
+            Affirmation(R.string.affirmations5),
+            Affirmation(R.string.affirmations6),
+            Affirmation(R.string.affirmations7),
+            Affirmation(R.string.affirmations8),
+            Affirmation(R.string.affirmations9),
+            Affirmation(R.string.affirmations10)
+        )
+    }
+}

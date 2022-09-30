59a79a689a6dde68b7a43ea6655ee294775ccbcf
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-09-30 11:23:33 +0545

    test viewmodels and livedata

--- Cupcake/app/src/test/java/com/example/cupcake/ViewModelTests.kt
+++ Cupcake/app/src/test/java/com/example/cupcake/ViewModelTests.kt
@@ -1,4 +1,25 @@
 package com.example.cupcake
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+
 class ViewModelTests {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    fun quantity_twelve_cupcake() {
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
+
+    fun price_12_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever { }
+        viewModel.setQuantity(12)
+        assertEquals("$27.00", viewModel.price.value)
+    }
 }

1e1f7f6fe7e2986df5c74660824be76865961dc4
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-10-01 08:53:04 +0545

    adaptive layouts

--- Training Sports/app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ Training Sports/app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.android.sports.databinding.FragmentSportsListBinding
 
 /**
@@ -31,12 +33,35 @@ import com.example.android.sports.databinding.FragmentSportsListBinding
 
 class SportsListFragment : Fragment() {
 
+    class SportsListOnBackPressedCallback(private val slidingPaneLayout: SlidingPaneLayout) :
+        OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+        SlidingPaneLayout.PanelSlideListener {
+
+        init {
+            slidingPaneLayout.addPanelSlideListener(this)
+        }
+
+        override fun handleOnBackPressed() {
+            slidingPaneLayout.closePane()
+        }
+
+        override fun onPanelSlide(panel: View, slideOffset: Float) {
+        }
+
+        override fun onPanelOpened(panel: View) {
+            isEnabled = true
+        }
+
+        override fun onPanelClosed(panel: View) {
+            isEnabled = false
+        }
+    }
+
     private val sportsViewModel: SportsViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         return FragmentSportsListBinding.inflate(inflater, container, false).root
     }
 
@@ -55,5 +80,11 @@ class SportsListFragment : Fragment() {
         }
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
+
+        val slidingPaneLayout = binding.slidingPaneLayout
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, SportsListOnBackPressedCallback(slidingPaneLayout)
+        )
     }
 }
